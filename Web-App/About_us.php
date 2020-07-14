0cc8f0a78e73a2173e3ff2ab05ce0aa9eff2d388
chsharma27 <root@localhost.localdomain>
2020-07-14 12:44:00 +0530

    further changes

--- Web-App/About_us.php
+++ Web-App/About_us.php
@@ -5,3 +5,4 @@ include 'header.html'
   <span class="content"><i>It is an initiative for providing better value of the produce to the farmer and better nutrition values to the end consumer by strngthening the weak supply and by bringing transparency in value chain.</i></span>
 </div>
 </header>
+<?php include 'Footer.html' ?>
--- Web-App/Login.php
+++ Web-App/Login.php
@@ -1,6 +1,14 @@
 <?php
 include 'header.html';
-include 'inc/login.php';
-$a = Login::isloggedin();
-echo "a = ",$a;
+//include 'inc/login.php';
 ?>
+<form action="inc/login.php" method="POST" id="login">
+  <h3 style="color:black">Log in!</h3>
+  <span style="color: black;font-size: 20px"> Fill Username or email and password</span><br>
+  <input type="text" name="userName" placeholder="Username"><br>
+  <input type="email" name="E-mail" placeholder="E-mail"><br>
+  <input type="password" name="pwd" placeholder="password"><br>
+  <button type="submit" name="login">Log in</button>
+</form>
+</header>
+<?php include 'Footer.html'?>
